e5a2583dbebd7c0467459bc1dd85c0b873f2d63c
Eyal Grinberg <eyalgrinberg@mail.tau.ac.il>
2024-06-21 17:56:18 +0300

    added 4 classes tests

--- classes_unit_tests.py
+++ classes_unit_tests.py
@@ -103,3 +103,36 @@ class TestFunction43(BaseTestCase):
         self.assertEqual(self.seven.get(), "111")
         self.assertEqual(self.one.get(), "1")
         self.assertEqual(self.zero.get(), "0")
+    
+class TestFunction44(BaseTestCase):
+    Point_2D = imported_functions[43]
+    
+    def setUp(self):
+        # Creating Point_2D instances for testing
+        self.a = Point_2D(1, 1)
+        self.b = Point_2D(0, 1)
+        self.c = Point_2D(-1, 1)
+        self.d = Point_2D(1, 1)
+
+    def test_repr(self):
+        self.assertEqual(repr(self.a), "Point(1, 1)")
+
+    def test_eq(self):
+        self.assertTrue(self.a == self.d)
+        self.assertFalse(self.a == self.b)
+
+    def test_add(self):
+        self.assertEqual(self.a + self.b, Point_2D(1, 2))
+
+    def test_sub(self):
+        self.assertEqual(self.a - self.b, Point_2D(1, 0))
+
+    def test_distance(self):
+        self.assertEqual(self.a.distance(self.c), 2)
+        self.assertAlmostEqual(self.a.distance(self.b), math.sqrt(1))
+
+    def test_angle_wrt_origin(self):
+        self.assertAlmostEqual(self.b.angle_wrt_origin(self.c), math.pi / 4)
+        self.assertAlmostEqual(self.c.angle_wrt_origin(self.b), 2 * math.pi - math.pi / 4)
+    
+    
